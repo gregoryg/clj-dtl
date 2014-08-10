63ebc5eaee5b446ddab16627ec21d78542ed6869
gregoryg <gregory@dynapse.com>
2014-08-10 20:32:35 +0000

    Removed unused functions. Added call to /dtl/tagged-tasks.  Response is now set to JSON content type

--- src/clj_dtl/handler.clj
+++ src/clj_dtl/handler.clj
@@ -13,12 +13,6 @@
 
 
 ;; from http://kendru.github.io/restful-clojure/2014/02/19/getting-a-web-server-up-and-running-with-compojure-restful-clojure-part-2/
-(defn- str-to [num]
-  (apply str (interpose ", " (range 1 (inc num)))))
-
-(defn- str-from [num]
-  (apply str (interpose ", " (reverse (range 1 (inc num))))))
-
 (defn set-dtl-file-path [path]
   (clj-dtl.api/set-dtl-file-path! path)
   (encode {:jobpath clj-dtl.api/dtl-job-path})
@@ -26,11 +20,10 @@
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/count-up/:to" [to] (str-to (Integer. to)))
-  (GET "/count-down/:from" [from] (str-from (Integer. from)))
   (GET "/dtl/path" [] (encode {:path dtl-job-path}))
   (GET "/dtl/setpath/:path" [path] (set-dtl-file-path path))
   (GET "/dtl/tasks" [] (encode {:tasks (get-tasks)}))
+  (GET "/dtl/tagged-tasks" [] (encode {:tasks (tagged-tasks)}))
   (GET "/dtl/node-data-array" [] (encode (gojs-node-data-array)))
   (GET "/dtl/link-data-array" [] (encode (gojs-link-data-array)))
   (route/resources "/")
@@ -47,12 +40,26 @@
       (assoc-in response [:headers "Access-Control-Allow-Origin"]
                 "*"))))
 
+(defn set-json-content
+  "middleware to set media type as application/json"
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (assoc-in response [:headers "Content-Type"]
+                "application/json; charset=utf-8"))))
+      ;; (if (contains? (:headers response) "Content-Type")
+      ;;   response
+      ;;   (content-type response "application/json; charset=utf-8"))
+      ;; response)))
+
 ;; (def app
 ;;   (handler/site app-routes))
 (def app
   (->
    (handler/site app-routes)
-   (allow-cross-origin)))
+   (allow-cross-origin)
+   (set-json-content)))
+
   
 ;; (defn start-server
 ;;   []
